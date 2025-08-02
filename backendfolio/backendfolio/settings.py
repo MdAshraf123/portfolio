58ba4ef2575cf02fa0760df01a5d742b60573be9
Md Ashraf <slopdgh1234567@gmail.com>
2025-08-02 15:09:50 +0530

    settings.py correction

--- backendfolio/backendfolio/settings.py
+++ backendfolio/backendfolio/settings.py
@@ -22,7 +22,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=config('EMAIL_PORT')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS=config('EMAIL_USE_TLS')
 EMAIL_HOST_USER=config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
