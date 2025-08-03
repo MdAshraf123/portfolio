0a9f0970bcbba6b0af2315a03ef57929a7d70653
Md Ashraf <slopdgh1234567@gmail.com>
2025-08-03 10:27:18 +0530

    app.jsx had some wrong import

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import Portfolio from './components/htmlelements/portfolio';
 import MainPage from './components/htmlelements/MainPage';
-import Skill from './components/htmlelements/Skill';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
