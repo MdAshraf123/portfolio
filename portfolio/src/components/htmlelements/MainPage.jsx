b2ea00265459d7a24e14f7600c3fe8d4b98020ca
Md Ashraf <slopdgh1234567@gmail.com>
2025-08-05 00:35:09 +0530

    some changes

--- portfolio/src/components/htmlelements/MainPage.jsx
+++ portfolio/src/components/htmlelements/MainPage.jsx
@@ -281,7 +281,7 @@ const MainPage=()=>{
                             <a href="mailto:mdashraf6776@gmail.com"  rel="noopener noreferrer"><Mail className="w-5 h-5 text-red-600" /></a>
                             <a href="https://www.linkedin.com/in/mdashraf123" target="_blank" rel="noopener noreferrer"><Linkedin className="w-5 h-5 text-blue-600" /></a>
                             <a href="https://github.com/MdAshraf123" target="_blank" rel="noopener noreferrer"><Github className="w-5 h-5 text-black" /></a>
-                            <a href="https://wa.me/+919576188048" target="_blank" rel="noopener noreferrer">
+                            <a href="" target="_blank" rel="noopener noreferrer">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="20"
