76e2c0d7d0d245af614e9317d7d0c5230a9a4581
Md Ashraf <slopdgh1234567@gmail.com>
2025-08-08 13:42:15 +0530

    static files collected

--- backendfolio/backendfolio/settings.py
+++ backendfolio/backendfolio/settings.py
@@ -154,6 +154,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
